b31b7181866a1fefe558f461926087e900ce5f4f
endotnick <endotnick@gmail.com>
2019-02-18 18:15:20 -0800

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,8 +4,19 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+    <h1>Directors Page</h1>
+    {directors.map(director => {
+    return (
+      <div>
+        <h3>Name: {director.name}</h3>
+        Movies:
+        <ul>
+          {director.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )})
+  }
+</div>
   );
 }
 
