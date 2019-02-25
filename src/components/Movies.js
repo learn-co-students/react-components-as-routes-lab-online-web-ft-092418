f7018c942bb1850e6d33a6360ed79bb58eebbb1a
github username <github email address>
2019-02-25 02:01:15 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,23 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+          {movies.map(
+            (m) => 
+            <div>
+                <h3>Title: {m.title}, Time: {m.time}</h3>
+                <ul>
+                  {/* <li>{m.genres.map((g) =>
+                    console.log(g.name)
+                    )}</li> */}
+                </ul>
+            </div>
+          )}
     </div>
   );
 };
 
 export default Movies;
+
+
+//need to get the genres to show
